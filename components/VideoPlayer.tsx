fc367d1f0a06e1700c6f726caa1ac88b16bc5d34
josh@jax <joshashep@gmail.com>
2023-11-06 22:33:43 -0500

    onmouseleave for dragselect

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -29,7 +29,9 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({
 }) => {
   const [isMouseDown, setIsMouseDown] = useState(false);
   const [mouseDownCoord, setMouseDownCoord] = useState<Coord | null>(null);
-  const [mouseDownTime, setMouseDownTime] = useState<number>(0); // Add this line to define mouseDownTime
+  const [mouseDownTime, setMouseDownTime] = useState<number>(0);
+  const clickThreshold = 200; // Time in milliseconds
+  const pixelThreshold = 10; // Pixel movement threshold for click detection
 
   useEffect(() => {
     if (videoRef.current) {
@@ -44,8 +46,15 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({
     return [x, y];
   };
 
+  const getMouseDistance = (start: Coord, end: Coord): number => {
+    const rect = videoRef.current!.getBoundingClientRect();
+    const dx = (end[0] - start[0]) * rect.width;
+    const dy = (end[1] - start[1]) * rect.height;
+    return Math.sqrt(dx * dx + dy * dy);
+  };
+
   const handleMouseDown = (event: React.MouseEvent) => {
-    setMouseDownTime(Date.now()); // Update the mouseDownTime here
+    setMouseDownTime(Date.now());
     const coord = getMousePosition(event);
     setMouseDownCoord(coord);
     setIsMouseDown(true);
@@ -55,12 +64,14 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({
     if (isMouseDown && mouseDownCoord) {
       const coord = getMousePosition(event);
       const timeElapsed = Date.now() - mouseDownTime;
-      if (timeElapsed < 200) { // Use the mouseDownTime to calculate the click duration
+      const mouseDistance = getMouseDistance(mouseDownCoord, coord);
+      
+      if (timeElapsed < clickThreshold && mouseDistance < pixelThreshold) {
         onClick(coord);
       } else {
         const rect: Rect = {
-          lo: [Math.min(coord[0], mouseDownCoord[0]), Math.min(coord[1], mouseDownCoord[1])],
-          hi: [Math.max(coord[0], mouseDownCoord[0]), Math.max(coord[1], mouseDownCoord[1])],
+          lo: mouseDownCoord,
+          hi: coord,
         };
         onDragEnd(rect);
       }
@@ -73,17 +84,38 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({
     if (isMouseDown && mouseDownCoord) {
       const coord = getMousePosition(event);
       const rect: Rect = {
-        lo: [Math.min(coord[0], mouseDownCoord[0]), Math.min(coord[1], mouseDownCoord[1])],
-        hi: [Math.max(coord[0], mouseDownCoord[0]), Math.max(coord[1], mouseDownCoord[1])],
+        lo: mouseDownCoord,
+        hi: coord,
       };
       onDrag(rect);
+    }
+  };
+
+  // Function to calculate the nearest edge
+  const snapToNearestEdge = (coord: Coord): Coord => {
+    const rect = videoRef.current!.getBoundingClientRect();
+    const x = coord[0] < 0.5 ? 0 : 1; // Snap to left (0) or right (1) edge based on x position
+    const y = coord[1] < 0.5 ? 1 : 0; // Snap to top (1) or bottom (0) edge based on y position (inverted y-axis)
+    return [x, y];
+  };
+
+  // Handler for when the mouse leaves the component
+  const handleMouseLeave = (event: React.MouseEvent) => {
+    if (isMouseDown && mouseDownCoord) {
+      const edgeCoord = snapToNearestEdge(getMousePosition(event));
+      const rect: Rect = {
+        lo: mouseDownCoord,
+        hi: edgeCoord,
+      };
+      onDragEnd(rect);
+      setIsMouseDown(false);
+      setMouseDownCoord(null);
     }
   };
 
   const handleKeyPress = (event: React.KeyboardEvent) => {
     onKeypress(event.key);
   };
-
   return (
     <div
       tabIndex={0}
@@ -91,6 +123,7 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
       style={{ outline: 'none', width, height }}
     >
       <video
